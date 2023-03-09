d8b9682b1ab8a9554bd366bc2fadd5b25ec0bfdd
DheerajSamanta <114645642+DheerajSamanta@users.noreply.github.com>
2023-03-09 16:09:26 +0530

    Update exp7_1.c

--- PPL_LAB_WORK/Experiment_7/exp7_1.c
+++ PPL_LAB_WORK/Experiment_7/exp7_1.c
@@ -1,1 +1,36 @@
 
+/* sum of all array elements using recursion */
+#include<stdio.h>
+int sum(int arr[],int n);
+void main(){
+
+
+  int n;
+  printf("How many numbers you want to enter?");
+  scanf("%d",&n);
+  int arr[n];
+  for(int i=0;i<n;i++){
+    printf("Enter %d number:",i+1);
+    scanf("%d",&arr[i]);
+
+  }
+  printf("arr={");
+  for(int j=0;j<n;j++){
+    printf("%d",arr[j]);
+
+  }
+  printf("}");
+  printf("\n Sum of the elements=%d",sum(arr,n));
+}
+
+int sum(int arr[],int n){
+  int summa=0;
+  static int k=0;
+  if(k>=n){
+    return 0;
+  }
+  summa=arr[k];
+  k++;
+  return summa=summa+sum(arr,n);
+}
+
